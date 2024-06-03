81ceb7322fe4bee26e78c2dff4e6d1f1521b349a
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-06-03 21:59:10 +0200

    ADD: migrated projects table and seeded it with faker

--- database/migrations/2024_06_03_172626_create_projects_table.php
+++ database/migrations/2024_06_03_172626_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 200);
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('url', 200);
             $table->timestamps();
         });
     }
